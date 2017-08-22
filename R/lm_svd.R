9432e086e7e68308e29986fbc1babfecfe93ba36
langendorfr <ryan.langendorf@colorado.edu>
2017-08-22 17:49:17 -0600

    lm_svd
    Updated the svd solver and future proofed it a little. Now includes
    intercept options and is better suited to accept input from the upcoming
    causal filter.

---
+++ R/lm_svd.R
@@ -0,0 +1,72 @@
+#' @title SVD solution to the Linear Least Squares Problem
+#'
+#' @description Solution to the linear least squares problem using Singular Value Decomposition (SVD). 
+#'
+#' @param y Target time series at time t+1.
+#' 
+#' @param x Time series at time t.
+#' 
+#' @param weights Weights for the exponentially-weighted linear model.
+#'
+#' @return Values in the Jacobian.
+#' 
+#' @references Deyle, E. R., May, R. M., Munch, S. B., & Sugihara, G. (2016, January). Tracking and forecasting ecosystem interactions in real time. In Proc. R. Soc. B (Vol. 283, No. 1822, p. 20152258). The Royal Society.
+#'
+#' @examples 
+#' x <- matrix(sample(1:10, size = 30, replace = TRUE), nrow = 10, ncol = 3)
+#' y <- 2*x[,1] + 0.25*x[,2] + rnorm(n = 10, mean = 0, sd = 1)
+#' weights <- apply(x, MARGIN = 1, FUN = var)
+#' 
+#' lm_svd(y, x, weights)
+#' 
+#' @export
+
+lm_svd <- function(y, x, weights, intercept = TRUE) 
+{
+  # Normalize weights
+  weights_normalized <- weights #/sum(weights)
+  
+  # Add column of ones for the constant term in the linear model unless intercept = FALSE
+  if (intercept == TRUE) {
+    A <- cbind(1, (x * weights_normalized))
+  } else {
+    A <- x * weights_normalized
+  }
+  
+  # Singular Value Decomposition
+  A_svd <- svd(A)
+  
+  s <- A_svd$d
+  
+  if (intercept == TRUE) {
+    s_inv <- matrix(0, nrow = (ncol(x)+1), ncol = (ncol(x)+1))
+  } else {
+    s_inv <- matrix(0, nrow = ncol(x), ncol = ncol(x))
+  }
+  
+  for(i in seq_along(s)){
+    if(s[i] >= max(s) * 1e-5) { # Remove small singular values
+      s_inv[i, i] <- 1/s[i]
+    }
+  }
+  
+  # Exponentially-weighted regression using singular value decomposition
+  coeff <- t(A_svd$v %*% s_inv %*% t(A_svd$u) %*% (weights_normalized * y))
+  
+  
+  if (intercept == TRUE) {
+    if (is.null(colnames(x)) == TRUE) {
+      colnames(coeff) <- c("Constant", colnames(data.frame(x)))  
+    } else {
+      colnames(coeff) <- c("Constant", colnames(x))
+    }
+  } else {
+    if (is.null(colnames(x)) == TRUE) {
+      colnames(coeff) <- colnames(data.frame(x))  
+    } else {
+      colnames(coeff) <- colnames(x)
+    }
+  }
+  
+  return(coeff)
+}
