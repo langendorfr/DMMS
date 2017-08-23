720b6e69341e761beecd8a9b0eaf050d037e031a
langendorfr <ryan.langendorf@colorado.edu>
2017-08-22 18:03:52 -0600

    Updated documentation

---
+++ R/lm_causalfilter.R
@@ -0,0 +1,26 @@
+#' @title Causal Filtering
+#'
+#' @description Applies supplied probabilities of causality to time series data.
+#' 
+#' @param x Time series at time t.
+#' 
+#' @param causal_probs Causal probabilities.
+#' 
+#' @param iteractions Number of times to apply the causal filter.
+#' 
+#' @param weights Weights for the exponentially-weighted linear model.
+#'
+#' @return Values in the Jacobian.
+#'
+#' @examples 
+#' 
+#' @export
+
+lm_causalfilter <- function(x, causal_probs, weights) 
+{
+
+  
+  
+  
+  
+}
